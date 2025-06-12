7edaa85eabc2e4ed28764621b3dc16dd3e53d7d9
Полонська Олександра <polonskaaleksandra@gmail.com>
2025-06-12 05:12:35 +0300

    Add files via upload

--- саперконсоль/саперконсоль/саперконсоль.cpp
+++ саперконсоль/саперконсоль/саперконсоль.cpp
@@ -65,44 +65,112 @@ void enter_players_names() {
 }
 
 //хто за ким ходить (1 гравець, 2 гравець)
-void game() {
-	bool valid = false;
-
-	while (!valid) {
-		int turn = -1;
+pair <int, int>  next_move() {
+		bool valid = false;
+		int cur_row, cur_col;
+		while (!valid) {
+			int turn = -1;
 			if (moves % 2 == 0) {
 				turn = 1;
 				cout << "Player1, ";
-			} else {
+			}
+			else {
 				turn = 2;
 				cout << "Player2, ";
 			}
-	}
-		
+		}
 
-	cout << "enter your move (row[1-9] column[1-9]):";
-	int cur_row, cur_col;
-	cin >> cur_row >> cur_col;
 
-	// задаємл обмеження, виводимо попередження
-	if (cur_row > 9 || cur_row < 1 || cur_col > 9 || cur_col < 1 || used[cur_row][cur_col]) {
-		cout << "Invalid move. Please, try again: ";
+		cout << "Enter your move (row[1-9] column[1-9]): ";
+		cin >> cur_row >> cur_col;
+
+		// задаємл обмеження, виводимо попередження
+		if (cur_row > 9 || cur_row < 1 || cur_col > 9 || cur_col < 1 || used[cur_row][cur_col]) {
+			cout << "Invalid move. Please, try again: ";
+		}
+		else {
+			valid = true;
+		}
 	}
-	else {
-		valid = true;
+	return { cur_row, cur_col };
+}
+
+	void recursion(int move_row, int move_col) {
+		for (int = 0; i < 8; ++i) {
+			if(board[move_row + row[i]][move_col + col[i]] > 0 && board[move_row + row[i]][move_col + col[i]] < 9 ){
+				used[board[move_row + row[i]][move_col + col[i]] = true;
+			} else if(board[move_row + row[i]][move_col + col[i]] == 0){
+				recursion(move_row + row[i], move_col + col[i]);
 	}
 }
+
+	void game() {
+		pair <int, int> move = next_move();
+		int move_row = move.first;
+		int move_col = move.second;
+		if (board[move_row][move_col] == 9) {
+			cout << " Oops! ";
+			if (moves % 2 == 0) {
+				turn = 1;
+				cout << "player1";
+			}
+			else {
+				turn = 2;
+				cout << "player2";
+			}
+			cout << "Lose. Would you like to try again? (Y/N): ";
+			char re;
+			cin >> re;
+			if (re == 'Y') {
+				restart();
+			}
+			else if (re == 'N') {
+				exit(0);
+			}
+			else {
+				bool good = false;
+				while (!good) {
+					cout << "You have entered wrong value. Answer again. Would you like to try again? (Y/N): "
+						cin >> re;
+					if (re == 'Y' || re == 'N') {
+						good == true;
+					}
+				}
+				if (re == 'Y') {
+					restart();
+				}
+				else if (re == 'N') {
+					exit(0);
+				}
+			}
+		}
+		else if (board[move_row][move_col] == 0) {
+			used[move_row][move_col] = true;
+			recursion(move_row, move_col);
+		} else {
+			used[move_row][move_col] = true;
+	}
+		moves++;
+		game();
+}
+
+	void restart() {
+		cout << "---------------------------------------------\nNew game:\n";
+
+		enter_players_names();
+
+		fill_board();
+
+		game();
+
+	}
+
 int main() {
 #ifdef LOCAL
 	freopen("input.in", "r", stdin);
 	freopen("output.in", "w", stdout);
 #endif
-
-	enter_players_names();
-
-	fill_board();
-
-	game();
+	restart();
 
 	print_board();
 	return 0;
